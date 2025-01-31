95ca981448d0bd36caf7088018b086a96ee9e0ef
Yukta <98662595+yuktaX@users.noreply.github.com>
2025-01-31 21:10:57 +0530

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+//@EnableSwagger2
+@ComponentScan(basePackages = {"controller", "platform.webservice.map", "platform.webservice.controller.base", "platform.webservice.controller.extended.LoginExtendedController"})
+//@ComponentScan(basePackages = {"controller", "platform.webservice.map", "platform.webservice.controller.base"})
+
 public class DemoApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Running Application....");
+		ApplicationContext ctx = SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
