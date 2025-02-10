3a8c4c776d34041424de99300b6272f405d30923
yuktaX <yukta.rajapur@gmail.com>
2025-02-10 13:14:13 +0530

    GET working

--- src/main/java/com/example/demo/travelApp/Registry.java
+++ src/main/java/com/example/demo/travelApp/Registry.java
@@ -5,23 +5,23 @@ import com.example.demo.travelApp.helper.*;
 import com.example.demo.travelApp.service.*;
 public class Registry {
 		public static void register(){
-				 HelperManager.getInstance().register(SuperAdminHelper.getInstance());
-				 HelperManager.getInstance().register(FlightHelper.getInstance());
-				 HelperManager.getInstance().register(BookingHelper.getInstance());
-				 HelperManager.getInstance().register(AirlineAdminHelper.getInstance());
-				 HelperManager.getInstance().register(PassengerHelper.getInstance());
-				 HelperManager.getInstance().register(DocumentHelper.getInstance());
-				 HelperManager.getInstance().register(AirlineHelper.getInstance());
-				 HelperManager.getInstance().register(LocationHelper.getInstance());
 				 HelperManager.getInstance().register(UserHelper.getInstance());
-				 ServiceManager.getInstance().register(new SuperAdminService());
-				 ServiceManager.getInstance().register(new FlightService());
-				 ServiceManager.getInstance().register(new BookingService());
-				 ServiceManager.getInstance().register(new AirlineAdminService());
-				 ServiceManager.getInstance().register(new PassengerService());
-				 ServiceManager.getInstance().register(new DocumentService());
-				 ServiceManager.getInstance().register(new AirlineService());
-				 ServiceManager.getInstance().register(new LocationService());
+				 HelperManager.getInstance().register(PassengerHelper.getInstance());
+				 HelperManager.getInstance().register(AirlineAdminHelper.getInstance());
+				 HelperManager.getInstance().register(LocationHelper.getInstance());
+				 HelperManager.getInstance().register(AirlineHelper.getInstance());
+				 HelperManager.getInstance().register(FlightHelper.getInstance());
+				 HelperManager.getInstance().register(SuperAdminHelper.getInstance());
+				 HelperManager.getInstance().register(DocumentHelper.getInstance());
+				 HelperManager.getInstance().register(BookingHelper.getInstance());
 				 ServiceManager.getInstance().register(new UserService());
+				 ServiceManager.getInstance().register(new PassengerService());
+				 ServiceManager.getInstance().register(new AirlineAdminService());
+				 ServiceManager.getInstance().register(new LocationService());
+				 ServiceManager.getInstance().register(new AirlineService());
+				 ServiceManager.getInstance().register(new FlightService());
+				 ServiceManager.getInstance().register(new SuperAdminService());
+				 ServiceManager.getInstance().register(new DocumentService());
+				 ServiceManager.getInstance().register(new BookingService());
 		}
 }
